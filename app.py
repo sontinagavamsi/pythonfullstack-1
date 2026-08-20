30ac83853fd05fbe290c46389d5c523bc28d92ff
sontinagavamsi <sontinagavamsi@gmail.com>
2026-08-20 10:47:51 +0000

    hy

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template ("about.html")
+
+@app . route("/contact")
+def contact():
+    retrun render_template("contact.html")
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html") 
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+ @app.route("/login")
+ def login():
+       
+
+if __name__=="__main__":
+    app.run(debug=True)
